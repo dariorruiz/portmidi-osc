e6f297f01d358f5110bfb8db3f047937452e6c9c
Antonio Ortega Brook <antonioortegabrook@me.com>
2024-03-04 23:43:20 -0300

    Prueba con Base.Threads

--- portmidi-osc-02.jl
+++ portmidi-osc-02.jl
@@ -1,6 +1,6 @@
 using PortMidi, Sockets, OpenSoundControl
+using Base.Threads
 
-begin
 # Initialize PortMidi
 PortMidi.Pm_Initialize()
 
@@ -11,18 +11,20 @@ for i in 0:PortMidi.Pm_CountDevices()-1
 end
 
 # Define the ID of your MIDI device (replace with your device ID)
-device_id = 1
+device_id = 0
 
 # Open the MIDI device as an input
 stream = Ref{Ptr{PortMidi.PortMidiStream}}(C_NULL)
-err = PortMidi.Pm_OpenInput(stream, device_id, C_NULL, 1024, C_NULL, C_NULL)
-if err != PortMidi.pmNoError
-    error(PortMidi.Pm_GetErrorText(err))
+begin
+	m_err = PortMidi.Pm_OpenInput(stream, device_id, C_NULL, 1024, C_NULL, C_NULL)
+	if m_err != PortMidi.pmNoError
+		error(unsafe_string(PortMidi.Pm_GetErrorText(m_err)))
+	end
 end
 
 # Define the CC number for your slider (replace with your slider CC number)
-slider_cc1 = 0x18
-slider_cc2 = 0x19
+slider_cc1 = 0x4a
+slider_cc2 = 0x47
 
 # Read MIDI messages
 events = Vector{PortMidi.PmEvent}(undef, 2)
@@ -30,41 +32,48 @@ last_value1 = 0
 last_value2 = 0
 current_value1 = 0
 current_value2 = 0
-end
 
-while true  # Loop forever
-    if PortMidi.Pm_Poll(stream[]) == PortMidi.pmGotData
-        err = PortMidi.Pm_Read(stream[], events, 1)
-        if err >= 1
-            msg = events[1].message
+poll = true
+@spawn :interactive while poll  # Loop forever
+	global last_value1
+	global last_value2
+	global current_value1
+	global current_value2
 
-	if PortMidi.Pm_MessageStatus(msg) == 0xB0 && (PortMidi.Pm_MessageData1(msg) == slider_cc1 || PortMidi.Pm_MessageData1(msg) == slider_cc2)
-		if PortMidi.Pm_MessageData1(msg) == slider_cc1
-			current_value1 = PortMidi.Pm_MessageData2(msg)
-		elseif PortMidi.Pm_MessageData1(msg) == slider_cc2
-			current_value2 = PortMidi.Pm_MessageData2(msg)
-		end
+	if PortMidi.Pm_Poll(stream[]) == PortMidi.pmGotData
+		err = PortMidi.Pm_Read(stream[], events, 1)
+        	if err >= 1
+        		msg = events[1].message
 
-		if current_value1 != last_value1 || current_value2 != last_value2
-			println("Slider 1 value: ", current_value1)
-			println("Slider 2 value: ", current_value2)
+			if PortMidi.Pm_MessageStatus(msg) == 0xB0 && (PortMidi.Pm_MessageData1(msg) == slider_cc1 || PortMidi.Pm_MessageData1(msg) == slider_cc2)
+				if PortMidi.Pm_MessageData1(msg) == slider_cc1
+					current_value1 = PortMidi.Pm_MessageData2(msg)
+				elseif PortMidi.Pm_MessageData1(msg) == slider_cc2
+					current_value2 = PortMidi.Pm_MessageData2(msg)
+				end
 
-			sock1 = UDPSocket()
-			msg1 = OpenSoundControl.message("/slider", "ii", Int32(current_value1), Int32(current_value2))
+				if current_value1 != last_value1 || current_value2 != last_value2
+					println("Slider 1 value: ", current_value1)
+					println("Slider 2 value: ", current_value2)
 
-			send(sock1, ip"127.0.0.1", 7777, msg1.data)
-			println(msg1)
+				#	sock1 = UDPSocket()
+				#	msg1 = OpenSoundControl.message("/slider", "ii", Int32(current_value1), Int32(current_value2))
 
-			last_value1 = current_value1
-			last_value2 = current_value2
+				#	send(sock1, ip"127.0.0.1", 7777, msg1.data)
+				#	println(msg1)
+
+					last_value1 = current_value1
+					last_value2 = current_value2
+				end
+			end
 		end
 	end
-
-
-        end
-    end
+	# Sleep for a short time to avoid hogging the CPU
+	PortMidi.Pt_Sleep(1)
 end
+poll = false
 
+println("...")
 
 # Don't forget to close the MIDI device when you're done
 PortMidi.Pm_Close(stream[])
